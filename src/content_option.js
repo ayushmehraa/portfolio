dd5cde071d28c24afd6c46e6c6d198f1ec73e7dc
Ayush Mehra <85395442+ayushmehraa@users.noreply.github.com>
2022-05-24 23:39:08 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "Ayush";
+const meta = {
+    title: "Ayush Mehra",
+    description: "I’m Ayush Mehra data scientist ,currently working in pernicia",
+};
+
+const introdata = {
+    title: "I’m Ayush Mehra",
+    animated: {
+        first: "I love Data science",
+        second: "I like to learn about new technolgiess",
+        third: "I like to work with ML",
+    },
+    description: "I like to learn about new technologies like machine learning, cloud , Augmented reality, etc currently i am exploring these domain one by one.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am in final year of Bachelor of computer application and i like to learn about data analysis.science and machine learing by myself currently i am doing internship at pernicia/pTeachTech    .",
+};
+const worktimeline = [{
+        jobtitle: "Data Analysis and Database Developer",
+        where: "pTeachTech",
+        date: "2022",
+    },
+    // {
+    //     jobtitle: "SQL Projects",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+    {
+        name: "Power BI",
+        value: 80,
+    },
+    {
+        name: "Data Analysis",
+        value: 74,
+    },
+    {
+        name: "Data Scrapping",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Data Scrapping",
+        description: "Data Scrapping is one of fudamental step of c.To do Data Science or analysis we need data and sometimes we gather that data by scrapping it through internet.",
+    },
+    {
+        title: "Data Analysis",
+        description: "Data Analysis is getting usefull insights from our raw data.It includes cleaing and analsing data by applying neccessary techniques and tools like statistics, Machine learnig, SQL, python, Power BI.",
+    },
+    {
+        title: "Power BI ",
+        description: "Power BI is another important tool for data analysis and data science project it majorly used for creating reports from our data which helps stackholders to get to know new updates and take decisiones.",
+    },
+
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mehrayush762@gmail.com",
+    YOUR_FONE: "9779814235",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ayushmehraa",
+    kaggle: "https://www.kaggle.com/ayushmehraa",
+    linkedin: "www.linkedin.com/in/ayushmehraa/",
+    // twitter: "https://twitter.com",
+    // youtube: "https://youtube.com",
+    // twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
